09f3599ab7cec7bcef35cbfec10e6acb5f66bf6e
coding-engineer-1701 <alexander.g.bush@gmail.com>
2025-08-24 15:26:45 +1000

    On Step 5

--- src/features/topics/topicsSlice.js
+++ src/features/topics/topicsSlice.js
@@ -1,3 +1,16 @@
 
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 
+const sliceOptions = {
+    name: 'topicsSlice', 
+    initialState: {
+        topics: {},
+    },
+    reducers: {
+        addTopic: (state, action) => {
+            const {id, name, icon} = action.payload;
+            state.topics[id] = action.payload;
+        }
+    }};
+
+export const topicsSlice = createSlice(sliceOptions);
